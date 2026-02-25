384ee2d2ddf682bcf1fe147e1a671a0e7d9bce36
Rebeccacheptoek <rebeccacheptoek1@gmail.com>
2026-02-25 10:37:51 +0300

    Added User, Wallet and Transaction models with relationships

---
+++ app/Models/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $fillable = ['name', 'email'];
+
+    public function wallets()
+    {
+        return $this->hasMany(Wallet::class);
+    }
+}
