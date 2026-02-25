c47f0c374b015296ff366e351ed507ea9c51f83e
Rebeccacheptoek <rebeccacheptoek1@gmail.com>
2026-02-25 10:37:51 +0300

    Defined API routes for users, wallets and transactions

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WalletController;
+use App\Http\Controllers\TransactionController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+
+Route::post('/wallets', [WalletController::class, 'store']);
+Route::get('/wallets/{wallet}', [WalletController::class, 'show']);
+
+Route::post('/transactions', [TransactionController::class, 'store']);
